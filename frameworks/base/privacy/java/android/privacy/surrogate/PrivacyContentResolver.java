d49d2a0e41f7564b2a527a233c2eb4f7c5054264
android <android@42fb184e-1d55-3cb9-6756-07e470ae625a>
2011-09-14 14:39:07 +0000

    removed unnecessary imports

--- frameworks/base/privacy/java/android/privacy/surrogate/PrivacyContentResolver.java
+++ frameworks/base/privacy/java/android/privacy/surrogate/PrivacyContentResolver.java
@@ -10,7 +10,6 @@ import android.provider.Browser;
 import android.provider.Calendar;
 import android.provider.CallLog;
 import android.provider.ContactsContract;
-import android.provider.Contacts.People;
 import android.provider.Telephony.Mms;
 import android.provider.Telephony.MmsSms;
 import android.provider.Telephony.Sms;
